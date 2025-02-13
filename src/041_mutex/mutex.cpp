32dafc43376e8370b7b5fca06613c72d6c7c5d55
AdnaneAbou <adnan.aboutalib2014@gmail.com>
2025-02-13 22:47:38 +0100

    Safe threads implementation mutex

--- src/041_mutex/mutex.cpp
+++ src/041_mutex/mutex.cpp
@@ -3,8 +3,18 @@
 #include <thread>
 #include <vector>
 
+int counter =0;
+std::mutex mtx; // Protects counter
+
+void incerement_safe(){
+    for(int i = 0; i < 1000000 ; i++){
+        std::lock_guard<std::mutex> lock(mtx);
+        counter++;
+    }
+}
 int main(){
     std::mutex m;
+
     auto print_thread_id = [&m](int id){
         std::lock_guard<std::mutex> lg(m);
         std::cout << "Printing from thread: " << id << "\n";
@@ -14,5 +24,14 @@ int main(){
     for(int i=0; i < 4; i+=1){
         my_threads.emplace_back(print_thread_id,i);
     }
+
+    std::thread t1(incerement_safe);
+    std::thread t2(incerement_safe);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Final counter " << counter << std::endl;
+
     return 0;
 }
